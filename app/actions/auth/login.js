c512894f8f5a6b3def13348e254d95f5c1d8a334
Dave Wasmer <davewasmer@gmail.com>
2017-01-04 23:30:12 -0700

    fix: allow all auth strategies on login action

--- app/actions/auth/login.js
+++ app/actions/auth/login.js
@@ -1,7 +1,11 @@
 import { mixin, Action, Response } from 'denali';
 import { authenticate } from '../../../lib';
 
-export default class LoginAction extends mixin(Action, authenticate()) {
+export default class LoginAction extends mixin(Action,
+  authenticate({ allowedStrategies: 'all' })
+) {
+
+  serializer = 'session';
 
   async respond(params) {
     let session = await this.currentUser.login(this, params);
